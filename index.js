753f24b75ed8da51b504a2585edf6bca64fc323c
pijushbarai <pijushbarai1@gmail.com>
2021-12-06 01:22:26 +0600

    carosel is coppied from w3 school

--- index.js
+++ index.js
@@ -1,11 +1,71 @@
 var element = document.getElementById("loginbtn");
-// element.addEventListener("click", function(){ alert("Hello World!"); });
 
-element.addEventListener("click",()=>{
-    alert("I am an alert box!");
-    element.innerHTML = "ball";
-    document.getElementById("passid").innerHTML = "ball";
-    // document.getElementById("name").innerHTML = "ball";
+
+var maindiv = document.getElementById("left_property");
+var login = document.getElementById("log_in_page");
+
+element.addEventListener("click", ()=> {
+    var maindiv = document.getElementById("left_property");
+    var login = document.getElementById("log_in_page");
+    login.style.display = "block";
+    // maindiv.style.display = "none"
+    maindiv.innerHTML = login.innerHTML;
+   alert("hi");
 });
 
+//////////////////////////////
+// script for carousel design
+///////////////////////////////
 
+
+//  by clicking change
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+// automatic change
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+}
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
