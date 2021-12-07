4d4ab90c78bfcf72907cd022fd846c735060f655
pijushbarai <pijushbarai1@gmail.com>
2021-12-08 00:48:36 +0600

    some unnesserarry code is removed

--- index.js
+++ index.js
@@ -9,63 +9,6 @@ element.addEventListener("click", ()=> {
     var login = document.getElementById("log_in_page");
     login.style.display = "block";
     // maindiv.style.display = "none"
-    maindiv.innerHTML = login.innerHTML;
-   alert("hi");
+    maindiv.textContent = login.value;
+   alert("hiii")
 });
-
-//////////////////////////////
-// script for carousel design
-///////////////////////////////
-
-
-//  by clicking change
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
-
-
-
-// automatic change
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-}
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
-}
